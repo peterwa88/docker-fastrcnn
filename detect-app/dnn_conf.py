20591a44d8c383b53107e9c9a18035188eecc3a8
peterwa88 <36631023+peterwa88@users.noreply.github.com>
2020-12-23 23:22:51 +0800

    Add files via upload

--- detect-app/dnn_conf.py
+++ detect-app/dnn_conf.py
@@ -1,7 +1,7 @@
 APP_PATH = "/docker-fastrcnn-main/detect-app"
 
 DNN_PATH = "/faster_rcnn_resnet50_coco_2018_01_28/frozen_inference_graph.pb"
-DNN_TXT_PATH = APP_PATH + "/faster_rcnn_resnet50_coco_2018_01_28.pbtxt"
+DNN_TXT_PATH = APP_PATH + "/opencv-extra/faster_rcnn_resnet50_coco_2018_01_28.pbtxt"
 DNN_LABELS_PATH = APP_PATH + "/opencv-extra/mscoco_label_map.pbtxt"
 
 LOG_PATH = APP_PATH + "/logs"
