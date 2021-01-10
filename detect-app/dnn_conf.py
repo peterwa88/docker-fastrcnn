b0ddcc72bc61c8b6cf983a867000828e2587c737
peterwa88 <36631023+peterwa88@users.noreply.github.com>
2021-01-10 17:58:47 +0800

    Update dnn_conf.py

--- detect-app/dnn_conf.py
+++ detect-app/dnn_conf.py
@@ -1,8 +1,8 @@
 APP_PATH = "/docker-fastrcnn-main/detect-app"
 
-DNN_PATH = "/faster_rcnn_resnet50_coco_2018_01_28/frozen_inference_graph.pb"
-DNN_TXT_PATH = APP_PATH + "/opencv-extra/faster_rcnn_resnet50_coco_2018_01_28.pbtxt"
-DNN_LABELS_PATH = APP_PATH + "/opencv-extra/mscoco_label_map.pbtxt"
+DNN_PATH = "/faster_rcnn_resnet50_coco_2021_01_10/frozen_inference_graph.pb"
+DNN_TXT_PATH = APP_PATH + "/opencv-extra/graph.pbtxt"
+DNN_LABELS_PATH = APP_PATH + "/opencv-extra/label_map.pbtxt"
 
 LOG_PATH = APP_PATH + "/logs"
 LOG_FILE = "dnn_rest.log"
